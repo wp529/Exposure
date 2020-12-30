abe652e9ae60a46ff8f88d3650dae1b3e38cc139
wangping <15680351591@163.com>
2020-12-30 20:42:03 +0800

    demo

--- app/src/main/java/com/wp/exposure/ListActivity.kt
+++ app/src/main/java/com/wp/exposure/ListActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.module.LoadMoreModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.wp.exposure.view.ExposureRecyclerView
 import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.android.synthetic.main.item_list.view.*
 
@@ -13,19 +15,60 @@ import kotlinx.android.synthetic.main.item_list.view.*
  * on 2020/11/6
  */
 class ListActivity : AppCompatActivity() {
+    private lateinit var adapter: ListAdapter
+    private lateinit var rvList: ExposureRecyclerView<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
-        val list = ArrayList<String>()
-        repeat(100) {
-            list.add("ListItemPosition$it")
+        rvList = findViewById(R.id.rvList)
+        initData()
+        srlRefreshLayout.setOnRefreshListener {
+            Log.d("ListActivity", "开始刷新列表")
+            srlRefreshLayout.postDelayed({
+                //模拟两秒才请求完数据
+                val list = ArrayList<String>()
+                repeat(20) {
+                    list.add("下拉刷新生成的数据$it")
+                }
+                adapter.setNewInstance(list)
+                srlRefreshLayout.isRefreshing = false
+                Log.d("ListActivity", "刷新数据更新完毕")
+            }, 2 * 1000L)
         }
-        rvList.adapter = ListAdapter(list)
-        rvList.exposureStateChangeListener = object : IExposureStateChangeListener {
-            override fun onExposureStateChange(position: Int, inExposure: Boolean) {
-                //这里一般用于数据统计SDK内部收集曝光
+    }
+
+    private fun initData() {
+        val list = ArrayList<String>()
+        repeat(20) {
+            list.add("初始化生成的数据$it")
+        }
+        adapter = ListAdapter(list).apply {
+            rvList.adapter = this
+            loadMoreModule.setOnLoadMoreListener {
+                Log.d("ListActivity", "开始加载更多数据")
+                rvList.postDelayed({
+                    //模拟两秒才请求完数据
+                    val moreData = ArrayList<String>()
+                    repeat(20) {
+                        moreData.add("加载更多生成的数据$it")
+                    }
+                    adapter.addData(moreData)
+                    Log.d("ListActivity", "加载更多数据添加完毕")
+                }, 2 * 1000L)
+            }
+            setOnItemClickListener { adapter, _, position ->
+                Log.d("ListActivity", "${adapter.data[position]}被删除了")
+                adapter.removeAt(position)
+            }
+        }
+        rvList.exposureStateChangeListener = object : IExposureStateChangeListener<String> {
+            override fun onExposureStateChange(
+                bindExposureData: String,
+                position: Int,
+                inExposure: Boolean
+            ) {
                 Log.i(
-                    "ListActivity", "position为${position}的item${
+                    "ListActivity", "${bindExposureData}${
                     if (inExposure) {
                         "开始曝光"
                     } else {
@@ -39,22 +82,20 @@ class ListActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        rvList.onResume()
+        rvList.onVisible()
     }
 
     override fun onPause() {
         super.onPause()
-        rvList.onPause()
-        //一般用于数据统计SDK内部不收集曝光
-        Log.i("ListActivity", "exposure record result: ${rvList.getAlreadyExposureData()}")
+        rvList.onInvisible()
     }
 }
 
-class ListAdapter(data: MutableList<String>) :
+class ListAdapter(data: MutableList<String>) : LoadMoreModule,
     BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_list, data) {
-
     override fun convert(holder: BaseViewHolder, item: String) {
         holder.itemView.apply {
+            exposureRoot.exposureBindData = item
             tvListText.text = item
         }
     }
